e0bf69f0e9f65ebabf2e29fe24a09513c455f9e8
RYan Olson <87497541+BootlegCash@users.noreply.github.com>
2025-05-12 14:14:32 -0700

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -26,6 +26,9 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from django.contrib.auth.models import User
 
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 def register(request):
     if request.user.is_authenticated:
