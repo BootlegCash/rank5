e61167580752cf32d856d92fd12867ffba516191
RYan Olson <87497541+BootlegCash@users.noreply.github.com>
2025-05-13 11:36:30 -0700

    Update urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
 from accounts.views import user_profile
+from rest_framework import routers
+from accounts.api import DailyLogViewSet
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+
+router = routers.DefaultRouter()
+router.register(r'log_drink', DailyLogViewSet, basename='log_drink')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,6 +18,9 @@ urlpatterns = [
     path('achievements/', include('achievements.urls')),
     path('competitions/', include('competitions.urls')),
     path('api/profile/', user_profile, name='user_profile'),
+
+    path('api/', include(router.urls)),
+    path('accounts/', include('accounts.urls')),
 
     # JWT token endpoints
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
