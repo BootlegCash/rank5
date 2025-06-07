d1cc6196155a161ccf85261703808dbd8b849fdc
Ryan Olson <87497541+BootlegCash@users.noreply.github.com>
2025-06-06 22:52:31 -0700

    Update apps.py

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,6 +1,12 @@
-from django.apps import AppConfig
+# In accounts/apps.py
 
+from django.apps import AppConfig
 
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
+
+    def ready(self):
+        # This line is the fix. It imports the signals (located in your models.py)
+        # when the app is ready, ensuring they are properly connected.
+        import accounts.models
