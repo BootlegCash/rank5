2c6ce996e1405df791dbf7fed9aea5d3b63f1cb1
Ryan Olson <87497541+BootlegCash@users.noreply.github.com>
2025-06-12 16:53:44 -0700

    Update settings.py

--- myapp/settings.py
+++ myapp/settings.py
@@ -70,7 +70,10 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 
 # Database
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600)
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 # Password validation
