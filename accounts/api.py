c19207731619f9c8ef106478e48bd87701d0c918
Ryan Olson <87497541+BootlegCash@users.noreply.github.com>
2025-06-15 02:43:46 -0700

    Update api.py

--- accounts/api.py
+++ accounts/api.py
@@ -1,7 +1,26 @@
 # accounts/api.py
 from rest_framework import viewsets, permissions
-from .models import DailyLog, current_log_date
+
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from .models import Profile
+from django.contrib.auth.models import User
 from .serializers import DailyLogSerializer
+from .models import DailyLog
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def user_profile(request):
+    profile = request.user.profile
+    return Response({
+        "id": request.user.id,
+        "username": request.user.username,
+        "email": request.user.email,
+        "xp": profile.xp,
+        "rank": profile.rank,
+        "alcohol_ml": profile.calculate_alcohol_drank()
+    })
 
 class DailyLogViewSet(viewsets.ModelViewSet):
     """
@@ -12,12 +31,10 @@ class DailyLogViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
-        # only the current day's log for this user
         today = current_log_date()
         return DailyLog.objects.filter(profile=self.request.user.profile, date=today)
 
     def perform_create(self, serializer):
-        # attach the profile and date automatically
         serializer.save(
             profile=self.request.user.profile,
             date=current_log_date()
