d450ecd7f7d380df5e5bac7f767b0a1813b2ba4a
Ryan Olson <87497541+BootlegCash@users.noreply.github.com>
2025-06-14 23:37:47 -0700

    Update serializers.py

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -5,7 +5,8 @@ from .models import DailyLog
 class DailyLogSerializer(serializers.ModelSerializer):
     class Meta:
         model = DailyLog
-        # only the fields we need from the Flutter client
-        fields = ['date', 'beer', 'floco', 'rum', 'whiskey', 'vodka', 'tequila',
-                  'shotguns', 'snorkels', 'thrown_up']
-        read_only_fields = ['date']
+        fields = [
+            'date', 'profile', 'beer', 'floco', 'rum', 'whiskey',
+            'vodka', 'tequila', 'shotguns', 'snorkels', 'thrown_up'
+        ]
+        read_only_fields = ['date', 'profile']
